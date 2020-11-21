d5652c5ccaf99e3256b1fa1427db10c3d4559ac0
m-habib <mohammad.habib.allah1@gmail.com>
2020-11-21 05:11:48 +0200

    added allCombinedPath to config

--- src/configuration.py
+++ src/configuration.py
@@ -1,16 +1,19 @@
-#Configurations file
+# Configurations file
+
 
 class Configuration:
     def __init__(self):
         self.dataPath = "../data"
         self.featuresPath = "../features"
         self.outputPath = "../output"
+        self.processStages = self.featuresPath + '/processStages.csv'
 
-        self.brandNamePath = self.featuresPath+'/brandNameDf.csv'
+        self.brandNamePath = self.featuresPath + '/brandNameDf.csv'
         self.containsColorPath = self.featuresPath + '/containsColorDf.csv'
         self.colorPath = self.featuresPath + '/colorDf.csv'
         self.containsMaterialPath = self.featuresPath + '/containsMaterialDf.csv'
         self.materialPath = self.featuresPath + '/materialDf.csv'
+        self.allCombinedPath = self.featuresPath + '/allCombinedDf.csv'
 
 
 config = Configuration()
